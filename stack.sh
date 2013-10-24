791fb631257b993db7993dcbc65c2699936ed855
Michael Crosby <crosby.michael@gmail.com>
2013-10-24 15:22:57 -0700

    Update stack example

--- stack.sh
+++ stack.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-RETHINK=$(docker run -e NAME=tododb -d crosbymichael/rethinkdb)
+RETHINK=$(docker run -e NAME=tododb -d -p 8080 crosbymichael/rethinkdb --bind all)
 docker link $RETHINK /rethinkdb
 
 docker run -rm -link /rethinkdb:db crosbymichael/linkwebapp --setup
 
-WEBAPP=$(docker run -p 5000 -link /rethinkdb:db crosbymichael/linkwebapp)
+WEBAPP=$(docker run -d -p 5000 -link /rethinkdb:db crosbymichael/linkwebapp)
 docker link $WEBAPP /todo
 
 docker ps -notrunc
