26efac34cac4f55bc1be6084aff71e32fbee3e55
Michael Crosby <crosby.michael@gmail.com>
2013-10-28 14:47:59 -0700

    Update output and stack for changes

--- stack.sh
+++ stack.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-RETHINK=$(docker run -e NAME=tododb -d -p 8080 crosbymichael/rethinkdb --bind all)
-docker link $RETHINK /rethinkdb
+docker run -e NAME=tododb -d -p 8080 -name rethinkdb crosbymichael/rethinkdb --bind all
+docker run -rm -link rethinkdb:db crosbymichael/linkwebapp --setup
 
-docker run -rm -link /rethinkdb:db crosbymichael/linkwebapp --setup
-
-WEBAPP=$(docker run -d -p 5000 -link /rethinkdb:db crosbymichael/linkwebapp)
-docker link $WEBAPP /todo
+docker run -d -p 5000 -link rethinkdb:db -name todo crosbymichael/linkwebapp
 
 docker ps
