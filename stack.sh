639cee078f3fc2a710607307401377f0a09ae817
Michael Crosby <crosby.michael@gmail.com>
2013-11-05 14:15:20 -0800

    Update stack for image name

--- stack.sh
+++ stack.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker run -e NAME=tododb -d -p 8080 -name rethinkdb crosbymichael/rethinkdb --bind all
-docker run -rm -link rethinkdb:db crosbymichael/linkwebapp --setup
-docker run -d -p 5000 -link rethinkdb:db -name todo crosbymichael/linkwebapp
+docker run -rm -link rethinkdb:db crosbymichael/frontend --setup
+docker run -d -p 5000 -link rethinkdb:db -name todo crosbymichael/frontend
 echo "\n"
 
 docker ps
